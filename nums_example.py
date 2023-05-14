1b4b47498101de2c733c767cc4e8d3c5ed2a5f53
Dmitri Camenev <dmit.cameneff@yandex.ru>
2023-05-14 13:56:32 +0300

    first commit

---
+++ nums_example.py
@@ -0,0 +1,15 @@
+nums = [12, 1, 5, 6, 7, 5]
+
+#type(nums)
+#print(len(nums))
+
+print(nums[0])
+
+print(nums[-1])
+
+#nums1 = nums[2:]
+#print(len(nums1))
+#print(nums1[1])
+#print(max(nums))
+ 
+print(list(sorted(nums)))
