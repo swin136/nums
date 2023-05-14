1305c38b7c4149c3c366fb1f0a2f309dad173d74
Dmitri Camenev <dmit.cameneff@yandex.ru>
2023-05-14 17:30:00 +0300

    Работа со слуяайными числами

--- numb_generator.py
+++ numb_generator.py
@@ -1,3 +1,11 @@
+"""
+Пример работы с гнераторами чисел
+А также с комментариями
+
+"""
+
+import random
+
 MAX_NUM = 6
 
 # Генерируем нечетные числа
@@ -19,3 +27,21 @@ print(f"Выводим обычные числа  >>> {nums_annual}")
 print(f"Выводим нечетные числа  >>> {nums}")
 #Четные числа
 print(f"Выводим нечетные числа  >>> {nums_2}")
+
+
+i = random.randint(120, 200)
+
+nums = [random.randint(1, 49), random.randint(50, 99), random.randint(100, 149), random.randint(150, 199),
+            random.randint(200, 249), random.randint(250, 299)]
+
+
+print(f"Случайное число {i}")
+
+print(f"Список случайных чисел >>> {nums}")
+
+nums = list(range(30, 41))
+print(f"Ещё список чисел >>> {nums}")
+
+
+
+
