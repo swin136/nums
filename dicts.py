61c2401a80382e2810f9d3eb93ac0fc1c5090041
Dmitri Camenev <dmit.cameneff@yandex.ru>
2023-05-16 16:53:03 +0300

    15052023

--- dicts.py
+++ dicts.py
@@ -17,13 +17,20 @@ morse_code_transl = ["-----", ".----", "..---", "...--", "....-", "....."]
 morse_code = dict(zip(morse_symbols, morse_code_transl))
 
 
-print(morse_code.get("4"))
-print(morse_code.get("2"))
-print(morse_code.get("1"))
+#print(f"Символ 4 >>> {str(morse_code.get("4"))}")
+
+sym = 5
+#sss = str(morse_code.get(str(sym)))
+
+print(f"В словаре символ {str(sym)} >>> {str(morse_code.get(str(sym)))}")
+
+
+#print(morse_code.get("2"))
+#print(morse_code.get("1"))
 
 
 
-if morse_code.get(" ") == None:
+if morse_code.get("12") == None:
     print("В словаре нифига нет!")
 
 
